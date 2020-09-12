dc1034a4a8777f6af0639fb27dfc196ba2619b99
girishsj11 <59494641+girishsj11@users.noreply.github.com>
2020-09-12 20:36:33 +0530

    Create Program_2.py

---
+++ Program_2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 20:33:09 2020
+
+@author: giri
+"""
+
+'''
+
+The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+
+1.The first line contains the sum of the two numbers.
+2.The second line contains the difference of the two numbers (first - second).
+3.The third line contains the product of the two numbers.
+
+'''
+
+if __name__ == "__main__":
+    a=int(input("Enter first number : "))
+    b=int(input("Enter second number : "))
+    print("Sum :",a+b)
+    print("Difference :",a-b)
+    print("Product :",a*b)
